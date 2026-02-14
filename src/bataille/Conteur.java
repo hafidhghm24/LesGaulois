f3f845b60d24322507662d6df56a783416a9d74f
Hafidhghm24 <hafidhghouilem@gmail.com>
2026-02-14 12:43:01 +0100

    avancement sur la classe conteur et Embuscade
    ajout du constructeur et methode conterHistoir() de la classe conteur
    
    correction de la classe Embuscade

--- src/bataille/Conteur.java
+++ src/bataille/Conteur.java
@@ -1,17 +1,18 @@
 package bataille;
 
-
-import bataille.Bataille;
-
 public class Conteur {
 	private String nom;
-	
+
 	public Conteur(String nom) {
 		this.nom = nom;
 	}
-	public void conterHistoire(Bataille bataille) {
-		System.out.println("je m'apelle " + nom + " et je vais vous conter une histoire");
-	}
-	
 
+	public void conterHistoire(Bataille bataille) {
+		System.out.println("Je suis " + nom + " Je vais vous conter une histoire qui se déroule en 50 avant Jésus-Christ," + " du temps où la Gaule est occupée par les Romains.\n");
+		System.out.println(bataille.decrireContexte());
+		System.out.println(bataille.choisirCombattants());
+		System.out.println(bataille.preparerCombat());
+		System.out.println(bataille.decrireCombat());
+		System.out.println(bataille.donnerResultat());
+	}
 }
--- src/bataille/Embuscade.java
+++ src/bataille/Embuscade.java
@@ -1,36 +1,42 @@
 package bataille;
 
+import personnages.*;
 import sites.Camp;
 import sites.Village;
 
-public class Embuscade implements Bataille{
+public class Embuscade implements Bataille {
 	private Village village;
 	private Camp camp;
 	private Gaulois[] promeneurs = new Gaulois[4];
-	private Soldat[] soldat = new Soldat[2];
+	private Soldat[] soldats = new Soldat[2];
+	
+	public Embuscade(Village village, Camp camp) {
+		this.village = village;
+		this.camp = camp;
+	}
 	
 	@Override
 	public String decrireContexte() {
-		
+		return "Dans une sombre forêt dans un coin reculé de la Gaule, quatre comparses se promènent.";
 	}
-	
+
 	@Override
 	public String choisirCombattants() {
-		
+		return "le combattant";
 	}
-	
+
 	@Override
 	public String preparerCombat() {
-		
+		return "preparerCombat";
 	}
-	
+
 	@Override
 	public String decrireCombat() {
-		
+		return "decrireCombat";
 	}
-	
+
 	@Override
 	public String donnerResultat() {
-		
+		return "donnerResultat";
 	}
 }
